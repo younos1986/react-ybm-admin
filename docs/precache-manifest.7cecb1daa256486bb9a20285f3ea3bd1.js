3ee99fa06b0d9fcc901c0145f8899262505c36f4
younes.bachaeimoghaddm <younes.bachaeimoghaddm@tui.be>
2020-04-20 12:39:09 +0200

    docs added

---
+++ docs/precache-manifest.7cecb1daa256486bb9a20285f3ea3bd1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698b4f3c616b23c90ff5d4a48a07802c",
+    "url": "/react-ybm-admin/index.html"
+  },
+  {
+    "revision": "503284f7460df0fb8a8d",
+    "url": "/react-ybm-admin/static/css/2.0911dc20.chunk.css"
+  },
+  {
+    "revision": "5dbf6fe229889da45e2a",
+    "url": "/react-ybm-admin/static/css/main.4b283e46.chunk.css"
+  },
+  {
+    "revision": "503284f7460df0fb8a8d",
+    "url": "/react-ybm-admin/static/js/2.7281bb89.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "/react-ybm-admin/static/js/2.7281bb89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dbf6fe229889da45e2a",
+    "url": "/react-ybm-admin/static/js/main.80682d18.chunk.js"
+  },
+  {
+    "revision": "5ea30f750aad36198677",
+    "url": "/react-ybm-admin/static/js/runtime-main.98207a88.js"
+  },
+  {
+    "revision": "833037b224e2093a48738f98c8a09cfa",
+    "url": "/react-ybm-admin/static/media/feat_slide1.833037b2.jpg"
+  },
+  {
+    "revision": "86116cc40a5e93de3fe3ef6d309222c1",
+    "url": "/react-ybm-admin/static/media/feat_slide2.86116cc4.jpg"
+  },
+  {
+    "revision": "6761ba35fb2a840ebb1a4671795cda86",
+    "url": "/react-ybm-admin/static/media/feat_slide3.6761ba35.jpg"
+  },
+  {
+    "revision": "72c550084cf34fb33778a17f1c41d3cd",
+    "url": "/react-ybm-admin/static/media/follow-img.72c55008.jpg"
+  },
+  {
+    "revision": "28b5ef6a57fc1dd5a02eea83a58fda0f",
+    "url": "/react-ybm-admin/static/media/home-bg1.28b5ef6a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ybm-admin/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d74cf0fc3d3ae0462546d564d0804397",
+    "url": "/react-ybm-admin/static/media/map.d74cf0fc.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53b038a0883719d89d639c7637769666.js"
+  "/react-ybm-admin/precache-manifest.7cecb1daa256486bb9a20285f3ea3bd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ybm-admin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
